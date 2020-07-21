2b4d1790cd6a7e3870b0faa2b95171c62eadceee
Henrique Vieira <hsfvieira@gmail.com>
2020-07-21 12:23:39 -0300

    First commit

--- run.sh
+++ run.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/zsh
 
-export DENO_INSTALL="/home/henrique/.deno"
-export PATH="$DENO_INSTALL/bin:$PATH"
-
-alias gen_cpf="deno run --allow-run /home/henrique/Projetos/gerador-cpf-cnpj-clipboard/index.js cpf"
+alias gen_cpf="deno run --allow-run $HOME/Projetos/gerador-cpf-cnpj-clipboard/index.js cpf"
 
 gen_cpf
