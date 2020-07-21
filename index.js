3b9e37e131b6d6dc34932b63663af9e19bb6d22c
Henrique Vieira <hsfvieira@gmail.com>
2020-07-21 12:51:19 -0300

    Adicionado gerar RG

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ import writeClipboard from './src/clipboard.js'
 
 import cpf from './src/cpf.js'
 import cnpj from './src/cnpj.js'
+import rg from './src/rg.js'
 
 const docs = {
     cpf,
-    cnpj
+    cnpj,
+    rg
 }
 
 const argTypeDoc = Deno.args[0]
---
+++ src/rg.js
@@ -0,0 +1,18 @@
+import { generateDigites } from './util.js'
+
+export default function generateCpf() {
+    const rgDigites = generateDigites(8)
+    const firstDig = calculateFirst(rgDigites)
+    return rgDigites + firstDig.toString()
+}
+
+function calculateFirst(rgDigites) {
+    const weights = [2, 3, 4, 5, 6, 7, 8, 9]
+    const listDigites = rgDigites.split('')
+
+    const calc = listDigites.reduce((acc, value, index) => {
+        return acc + (value * weights[index])
+    }, 0)
+
+    return calc % 11 < 2 ? 0 : 11 - (calc % 11)
+}
